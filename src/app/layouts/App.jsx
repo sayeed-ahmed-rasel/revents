7d242a70b92183b3f042866636f404b7193d52dc
sayeed-ahmed-rasel <asayeed303@gmail.com>
2019-03-20 23:27:11 +0600

    added sementic ui and test with icon and button :)

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>hello from home page!</h1>
+        <h1>hello!</h1>
+
+         <button className="ui icon button">
+          <i className="smile icon" />
+          css button
+        </button>
+        <Button icon="smile" content="react button" />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/layouts/App';
-import * as serviceWorker from './serviceWorker';
-const rootEle = document.getElementById('root');
-const render = ()=>{
-    ReactDOM.render(<App/>, rootEle)
+import React from "react";
+import ReactDOM from "react-dom";
+//import "semantic-ui-css/semantic.min.css";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layouts/App";
+import * as serviceWorker from "./serviceWorker";
+const rootEle = document.getElementById("root");
+const render = () => {
+  ReactDOM.render(<App />, rootEle);
+};
+
+if (module.hot) {
+  module.hot.accept("./app/layouts/App", () => {
+    setTimeout(render);
+  });
 }
 
-if(module.hot){
-    module.hot.accept('./app/layouts/App' , ()=>{
-        setTimeout(render)
-    })
-}
-
-render()
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
